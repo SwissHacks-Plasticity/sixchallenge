d0c6c8d9a1236157bc87071823dce4f0091d86cc
Pascal Fivian <pascal.fivian@3ap.ch>
2024-06-29 11:03:00 +0200

    added fonts and colors to tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,19 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    colors: { lightblue: '#7395EB', blue: '#4242D2', green: '#B7B722', lightgreen: '#D6DB47' },
+    fontFamily: {
+      sans: ['var(--font-poppins)'],
+    },
+    fontWeight: {
+      normal: '300',
+      bold: '700',
     },
   },
   plugins: [],
