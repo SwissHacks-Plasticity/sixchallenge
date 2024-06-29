02e3a5272cbb2decd99367050e6dc93dae0d321c
Pascal Fivian <pascal.fivian@3ap.ch>
2024-06-29 12:45:58 +0200

    link compensator to action button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,16 @@
 import Image from 'next/image';
 import Autocomplete from './components/autocomplete';
 import { PlasticCalculatorWizard } from './components/PlasticCalculatorWizard/PlasticCalculatorWizard';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 background-gradient-blue">
       <div className="z-10 flex flex-col items-center justify-center">
         <img src="/Logo_gross.svg" alt="Vercel Logo" className="w-full h-full" />
-        <button>Take Action</button>
+        <Link href="/compensator" className="button">
+          Take Action
+        </Link>
       </div>
     </main>
   );
