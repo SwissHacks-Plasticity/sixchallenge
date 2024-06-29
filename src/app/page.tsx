9cfc6bcd71783683577eb87c3a3cc2226e2c567d
Pascal Fivian <pascal.fivian@3ap.ch>
2024-06-29 13:50:24 +0200

    fix landing logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,8 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 background-gradient-blue">
-      <div className="z-10 flex flex-col items-center justify-center">
-        <img src="/Logo_gross.svg" alt="Vercel Logo" className="w-full h-full" />
+      <div className="z-10 w-full flex flex-col items-center justify-center">
+        <img src="/Logo gross.svg" alt="Vercel Logo" className="w-2/3 mb-10" />
         <Link href="/compensator" className="button">
           Take Action
         </Link>
