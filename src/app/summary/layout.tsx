26c4690ba9a12269cb08fdf0708e4a134c3c4709
Pascal Fivian <pascal.fivian@3ap.ch>
2024-06-29 21:56:38 +0200

    wrapp summary in suspense boundary

--- src/app/summary/layout.tsx
+++ src/app/summary/layout.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import { Suspense } from 'react';
 
 export default function SummaryLayout({
   children,
@@ -8,7 +9,7 @@ export default function SummaryLayout({
   return (
     <>
       <main className="flex flex-col min-h-screen background-gradient-onlyblue text-white">
-        {children}
+        <Suspense>{children}</Suspense>
       </main>
 
       <footer className="bg-darkBlue flex items-center flex-col text-xl text-white">
--- src/app/summary/page.tsx
+++ src/app/summary/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { useSearchParams } from 'next/navigation';
 import { Card } from '../components/card';
+import { Suspense } from 'react';
 
 export default function Summary() {
   const searchParams = useSearchParams();
